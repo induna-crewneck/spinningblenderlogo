8961bdc5cd159b27c76a0b7e178bce72c6119d85
induna-crewneck <66157637+induna-crewneck@users.noreply.github.com>
2022-04-09 14:28:22 +0200

    Update blender_loading_counter.bat

--- blender_loading_counter.bat
+++ blender_loading_counter.bat
@@ -3,7 +3,7 @@ REM   ===============================================================
 REM   ==================== SPINNING BLENDER LOGO ====================
 REM   ================= github.com/induna-crewneck/ =================
 REM   ===============================================================
-mode con: cols=65 lines=20
+mode con: cols=65 lines=22
 color 6
 set counter=0
 :LOOP
